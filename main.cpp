a2296803103765c1a31aef7fcd3b7ef6ecc7d45a
Du Park <dupark3@gmail.com>
2018-02-22 12:11:14 -0500

    Pass input line to split function and print each word.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <string>
 
-using namespace std;
+int main() {
+    std::cout << "Enter string to split into individual words: ";
+    std::string s;
+    while (getline(std::cin, s)){
+        std::vector<std::string> splitWords = split(s);
 
-int main()
-{
-    cout << "Hello world!" << endl;
+        for (std::vector<std::string>::size_type i = 0; i != splitWords.size(); ++i)
+            std::cout << splitWords[i] << std::endl;
+    }
+
     return 0;
 }
