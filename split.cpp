0588206378df3da2132aec8cbec0388f887a734a
Du Park <dupark3@gmail.com>
2018-02-22 12:21:45 -0500

    Split function outline with iterators

---
+++ split.cpp
@@ -0,0 +1,31 @@
+#include <algorithm> // find_if
+#include <cctype> //isspace
+#include <string>
+#include <vector>
+
+typedef std::string::iterator string_iter;
+
+bool notSpace (const char& c){
+    return !isspace(c);
+}
+
+bool space (const char& c){
+    return isspace(c);
+}
+
+std::vector<std::string> split(const std::string& line){
+    std::vector<std::string> ret;
+    string_iter i = line.begin();
+
+    while (i != line.end()){
+        std::find_if(i, line.end(), notSpace); // i now points at the first non-space char
+        string_iter j = std::find_if(i, line.end(), space); // j now points at the next space char
+        if (i != line.end())
+            ret.push_back(std::string(i, j));
+        i = j;
+    }
+
+
+
+    return ret;
+}
