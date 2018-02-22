7517c1c71470d571c5faa1e1481b91837b06d34f
Du Park <dupark3@gmail.com>
2018-02-22 12:43:29 -0500

    Use Copy to store all returned vector from split for all lines of input.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <algorithm> //copy
 #include <iostream>
+#include <iterator> //back_inserter
 #include <string>
 #include <vector>
 
@@ -7,15 +9,17 @@
 int main() {
     std::cout << "Enter string to split into individual words: ";
     std::string s;
+    std::vector<std::string> splitWords;
 
     // Read one line at a time, passing it to split, which will return a vector separating the line by each space
     while (getline(std::cin, s)){
-        std::vector<std::string> splitWords = split(s);
-
-        // print each word on a new line
-        for (std::vector<std::string>::size_type i = 0; i != splitWords.size(); ++i)
-            std::cout << splitWords[i] << std::endl;
+        std::vector<std::string> temp = split(s);
+        copy(temp.begin(), temp.end(), back_inserter(splitWords));
     }
+
+    // print each word on a new line
+    for (std::vector<std::string>::size_type i = 0; i != splitWords.size(); ++i)
+        std::cout << splitWords[i] << std::endl;
 
     return 0;
 }
